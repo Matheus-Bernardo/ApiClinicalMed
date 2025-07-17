aff621bbaa26ff10f04daa31117d9e3805ef87c5
Matheus Bernardo <matheus.mh@ges.inatel.br>
2025-07-17 14:34:41 -0300

    :sparkles: feat new endpoint Login

---
+++ WebApplication1/WebApplication1/Controllers/LogoutController/LogoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers.LogoutController;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class LogoutController:ControllerBase
+{
+    [HttpPost]
+    public IActionResult Logout()
+    {
+        
+        Response.Cookies.Append("jwt", "", new CookieOptions
+        {
+            HttpOnly = true,
+            Secure = true,
+            SameSite = SameSiteMode.Strict,
+            Expires = DateTimeOffset.UtcNow.AddDays(-1)
+        });
+
+        return Ok(new { message = "Logout efetuado com sucesso" });
+    }   
+}
